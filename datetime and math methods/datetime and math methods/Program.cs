e19bd05f121ff92063118e236d454b7c9ff6f3e9
Mevre <merve.unity@gmail.com>
2023-07-31 22:40:48 +0300

    math notes

---
+++ datetime and math methods/datetime and math methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datetime_and_math_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Math.Abs(-5)); // mutlak degeri verir
+
+            Console.WriteLine(Math.Sin(10)); // 10un sinusu
+            Console.WriteLine(Math.Cos(10)); // 10un cosinusu
+            Console.WriteLine(Math.Tan(10)); // 10un tanjantı
+
+
+            Console.WriteLine(Math.Ceiling(21.1));// kusurati ne olursa olsun bir ust tam sayiyi verir baglamda 22
+            Console.WriteLine(Math.Round(21.3)); // matematikteki yuvarlama islemini yapar hangi tam sayiya yakinsa ona yuvarlar yani
+            Console.WriteLine(Math.Floor(21.9)); // kusurat ne olursa olsun verilen tami verir. Baglamd 21
+
+            Console.WriteLine(Math.Max(5, 8)); //iki sayi arasinan en buyugu getirir.
+            Console.WriteLine(Math.Min(5, 8)); // iki sayi arasindan en kucugu getirir.
+
+            Console.WriteLine(Math.Pow(5, 8));// power. sayi ussu alir. 5 üssü 8 sonucunu getirir.
+            Console.WriteLine(Math.Sqrt(12)); // 12nin karekokunu alir
+            Console.WriteLine(Math.Log(9)); //9un e tabanindaki logaritmasini alir (e tabani??)
+            Console.WriteLine(Math.Exp(3));// e üssü 3
+        }
+    }
+}
---
+++ datetime and math methods/datetime and math methods/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("datetime and math methods")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("datetime and math methods")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("4e6b6e9b-0c9f-42d9-9dc7-5d9e7d4c6dbf")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
