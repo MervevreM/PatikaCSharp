008a9e1181082ff5406621f39ce442b0b6232e3d
Mevre <merve.unity@gmail.com>
2023-08-11 20:30:57 +0300

    homework of collections part 2

--- collections question2/collections question2/Program.cs
+++ collections question2/collections question2/Program.cs
@@ -5,7 +5,7 @@ namespace collections_question2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             ArrayList numbers = new ArrayList();
 
